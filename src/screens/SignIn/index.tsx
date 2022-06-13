af9095b4e116dbd1ed498ba26f632f19338feef8
Ericlys Moreira <ericlysm@gmail.com>
2022-06-13 19:06:54 -0300

    feat: adding SignInSocialButton on SignIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { RFValue } from "react-native-responsive-fontsize";
 
+import AppleSvg from '../../assets/apple.svg';
+import GoogleSvg from '../../assets/google.svg';
 import LogoSvg from '../../assets/logo.svg';
+
+import { SignInSocialButton } from "../../components/SignInSocialButton";
 
 import {
   Container,
   Footer,
+  FooterWrapper,
   Header,
   SignInTitle,
   Title,
@@ -36,7 +41,16 @@ export function SignIn(){
       </Header>
 
       <Footer>
-
+        <FooterWrapper>
+          <SignInSocialButton
+            title="Entrar com Google"
+            svg={GoogleSvg}
+          />
+          <SignInSocialButton
+            title="Entrar com Apple"
+            svg={AppleSvg}
+          />
+        </FooterWrapper>
       </Footer>
     </Container>
   )
